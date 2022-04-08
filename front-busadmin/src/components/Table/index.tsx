df0f8ff7fc54112811eb4ea4c53a46973bdf481f
lucas-chave <lcchavesr150@gmail.com>
2022-04-07 22:57:06 -0300

    feat: adding styles in table

--- front-busadmin/src/components/Table/index.tsx
+++ front-busadmin/src/components/Table/index.tsx
@@ -1,5 +1,49 @@
-export const Table = () => {
+import { ContainerTable } from './styles';
+
+interface Props {
+  thead: string[];
+  tbody: any[];
+};
+
+export const Table = ({ thead, tbody }: Props) => {
   return (
-    <table></table>
+    <ContainerTable>
+      <table>
+        <thead>
+          <tr>
+            {thead.map((title) => (
+              <th>
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tbody.map((driver) => (
+            <>
+              <tr>
+                <td>{driver.full_name}</td>
+                <td>{driver.cpf}</td>
+                <td>{driver.birthday}</td>
+                <td>{driver.cellphone_one}</td>
+              </tr>
+              <tr>
+                <td>{driver.full_name}</td>
+                <td>{driver.cpf}</td>
+                <td>{driver.birthday}</td>
+                <td>{driver.cellphone_one}</td>
+              </tr>
+              <tr>
+                <td>{driver.full_name}</td>
+                <td>{driver.cpf}</td>
+                <td>{driver.birthday}</td>
+                <td>{driver.cellphone_one}</td>
+              </tr>
+          </>
+
+          ))}
+        </tbody>
+      </table>
+    </ContainerTable>
   );
 };
--- front-busadmin/src/components/app/drivers/index.tsx
+++ front-busadmin/src/components/app/drivers/index.tsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks"
+import { fetchDrivers } from "../../../store/slices/drivers/midleware";
 import { DashboardPage } from "../../dashboardPage"
 import { Table } from "../../Table"
 
 export const Drivers = () => {
+  const dispatch = useAppDispatch();
+
+  const { drivers } = useAppSelector((state) => state.drivers );
+
+  console.log(drivers);
+
+  const thead = [
+    'Nome',
+    'Documento',
+    'Data de nascimento',
+    'Celular',
+  ];
+
+  useEffect(() => {
+    dispatch(fetchDrivers());
+  }, []);
+
   return (
     <DashboardPage title="Motoristas">
-      <Table />
+      <Table thead={thead} tbody={drivers} />
     </DashboardPage>
   )
 }
