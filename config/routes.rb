0421987d048bf49c4a4902ee191a848550c67d32
Joao Chaves <chavesjj@outlook.com>
2022-04-18 23:52:08 -0300

    (backend) - create simple jwt login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resource :users, only: [:create]
   resource :drivers, only: %i[create show update]
+  post '/login', to: 'users#login'
   get '/drivers/list', to: 'drivers#list'
-  post '/auth/login', to: 'authentication#login'
-  #post '/login', to: 'users#login'
-  #get '/auto_login', to: 'users#auto_login'
 end
