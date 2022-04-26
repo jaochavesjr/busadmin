dea0325f09498384b8bb72586b0ae715af0c1c2c
Joao Chaves <chavesjj@outlook.com>
2022-04-26 14:04:33 -0300

    fix routes travels

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :drivers, only: %i[create show update]
   resource :clients, only: %i[create show update]
   resource :buses, only: %i[create show update]
-  resource :travel, only: %i[create show update]
+  resource :travels, only: %i[create show update]
   post '/login', to: 'users#login'
   get '/drivers/list', to: 'drivers#list'
   get '/clients/list', to: 'clients#list'
